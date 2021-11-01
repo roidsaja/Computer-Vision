cbdd8ed83a14514abeb35a20e7d1e0bf1f3cee7a
keitatofu <roidthebeast@gmail.com>
2021-11-01 15:05:03 +0300

    polynomial regression

--- polynomialRegression/polyReg.py
+++ polynomialRegression/polyReg.py
@@ -15,18 +15,21 @@ cases = []
 columns = dataset.keys()
 confirmed = dataset.iloc[:, 4:777]
 confirmed_dates = confirmed.keys()
+indonesiaCases = []
 
 for i in confirmed_dates:
     confirmed_sum = confirmed[i].sum()
     cases.append(confirmed_sum)
+    indonesiaCases.append(dataset[dataset['Country/Region']=='Indonesia'][i].sum())
 
 day_initial = np.array( [i for i in range(len(confirmed_dates)) ]).reshape(-1, 1)
 cases = np.array(cases).reshape(-1, 1)
+indonesiaCases = np.array(indonesiaCases).reshape(-1, 1)
 
-future_day = 20
+future_day = 200
 future_predict = np.array( [i for i in range(len(confirmed_dates) + future_day) ]).reshape(-1, 1)
 #print(future_predict)
-dates_adjust = future_predict[:-future_day]
+dates_adjust = future_predict[:-10]
 #print(dates_adjust)
 
 start = '1/22/2020'
@@ -34,26 +37,33 @@ date_start = datetime.datetime.strptime(start, '%m/%d/%Y')
 date_future = []
 for i in range(len(future_predict)):
     date_future.append( (date_start + datetime.timedelta(days = i)).strftime('%m/%d/%Y'))
+
 #print(date_future)
 
 # set test_size to 15% of the original data, set shuffle to False since the model needs to learn from the trend of the data
-X_train_confirmed, X_test_confirmed, y_train_confirmed, y_test_confirmed = train_test_split(day_initial, cases, test_size=0.15, shuffle=False)
+X_train_confirmed, X_test_confirmed, y_train_confirmed, y_test_confirmed = train_test_split(day_initial, indonesiaCases, test_size=0.15, shuffle=False)
 
 # transform data for polynomial regression
-poly = PolynomialFeatures(degree=5)
+poly = PolynomialFeatures(degree=10)
 poly_X_train_confirmed = poly.fit_transform(X_train_confirmed)
+print(poly_X_train_confirmed)
 poly_X_test_confirmed = poly.fit_transform(X_test_confirmed)
 polyPredictFuture = poly.fit_transform(future_predict)
 
 # polynomial regression
-lm = LinearRegression(normalize=True, fit_intercept=True)
+lm = LinearRegression()
 lm.fit(poly_X_train_confirmed, y_train_confirmed)
 testLinearPredict = lm.predict(poly_X_test_confirmed)
 linearPredict = lm.predict(polyPredictFuture)
 print('MAE:', mean_absolute_error(testLinearPredict, y_test_confirmed))
 print('MSE', mean_squared_error(testLinearPredict, y_test_confirmed))
 
+plt.figure(1)
 plt.plot(testLinearPredict)
 plt.plot(y_test_confirmed)
 plt.legend(['Confirmed Cases', 'Linear Prediction'], prop={'size':10})
+
+plt.figure(2)
+plt.plot(indonesiaCases)
+
 plt.show()
